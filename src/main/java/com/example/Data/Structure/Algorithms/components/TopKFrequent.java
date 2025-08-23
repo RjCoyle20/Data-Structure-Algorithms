94042917a2edfd465e0d4440fa3da087314104d0
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2025-08-22 20:34:09 -0500

    wrote documentation/comments talking through the process of the brute force solution.

--- src/main/java/com/example/Data/Structure/Algorithms/components/TopKFrequent.java
+++ src/main/java/com/example/Data/Structure/Algorithms/components/TopKFrequent.java
@@ -7,21 +7,29 @@ import java.util.Map;
 
 public class TopKFrequent {
     public int[] topKFrequent(int[] nums, int k) {
+        // BRUTE FORCE SOLUTION
+        // create a Hashmap named count
         Map<Integer, Integer> count = new HashMap<>();
 
+        //iterate through our nums, add them to Hashmap count with a key matching the number and a value representing the occurrences of the number, defaulting to 0 if this is its first time being seen in the array.
         for (int num : nums) {
             count.put(num, count.getOrDefault(num, 0) +1);
         }
+        // create a new List of integer arrays named arr. These arrays will only hold two numbers, the frequency of occurrence and number
         List<int[]> arr = new ArrayList<>();
-
-
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()){
+        // add new integer arrays to the ArrayList arr. These consist of the [frequency, number]
+        for (Map.Entry<Integer, Integer> entry: count.entrySet()) {
             arr.add(new int[] {entry.getValue(), entry.getKey()});
         }
-        arr.sort((a,b) -> b[0] - a[0]);
+        // sort the List in descending order by the frequency
+        arr.sort((a, b) -> b[0] - a[0]);
+
+        //create a new integer array named res for the result. Its length will be the k number of numbers we're looking for
         int[] res = new int[k];
-        for (int i = 0; i < k; i++) {
-            res[i]= arr.get(i)[1];
+
+        //iterate through our list of arrays arr, assigning the second number in the arrays to the same index in array res.
+        for (int i = 0; i < k ; i++) {
+            res[i] = arr.get(i)[1];
         }
         return res;
     }
